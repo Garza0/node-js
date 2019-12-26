98dac5665f7e7d5db25cc382955286d919aa1dde
Garza0 <konovalovandrey7@gmail.com>
2019-12-26 19:42:47 +0200

    done

--- index.js
+++ index.js
@@ -36,6 +36,19 @@ const server = http.createServer((req, res) => {
 
   let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
   const ext = path.extname(filePath)
+  let contentType = 'text/html'
+
+  switch (ext) {
+    case '.css':
+      contentType = 'text/css'
+      break
+    case '.js':
+      contentType = 'text/javascript'
+      break
+    default:
+      contentType = 'text/html'
+  }
+
 
   if (!ext) {
     filePath += '.html'
@@ -55,15 +68,16 @@ const server = http.createServer((req, res) => {
       })
     } else {
       res.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': contentType
       })
       res.end(content)
     }
   })
 })
 
+const PORT = process.env.PORT || 3000
 
-server.listen(3000, () => {
-  console.log('Server has been started')
+server.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT}`)
 })
 
